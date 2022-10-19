1eedaa60d132cc68a14bb3fcd86125ee30b8ffe5
Jose Mendoza <developermendoza@gmail.com>
2022-10-19 10:41:55 -0700

    add fetch api to get list from heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    fetch("https://task-app123465.herokuapp.com/")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => setTodoList(data))
+      .catch((error) => console.log("error: ", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo._id}>{todo.item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
